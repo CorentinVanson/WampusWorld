876d67236d8412273e85ef4cf25e9bcb9f92e0a8
Corentin <corentin.vanson@isen-lille.fr>
2016-04-05 00:51:42 -0400

    second

--- Wumpus.pl
+++ Wumpus.pl
@@ -35,8 +35,11 @@
 start :-
 	initialise,
         new(P, auto_sized_picture('Wumpus World')),
-	afficherJeu(P),
-	afficherJeu(P).
+	boucleDeJeu(P).
+
+/*******************************************/
+/**************INITIALISATION***************/
+/*******************************************/
 
 initialise :-
 	initialisation,
@@ -62,11 +65,9 @@ initialisation :-
 	retractall(maybeTrou([_,_])),
 	retractall(sureAscenseur([_,_])),
 	retractall(sureMonstre([_,_])),
-	retractall(sureMonstreMort([_,_])),
 	retractall(sureTrou([_,_])),
 	retractall(sureNotAscenseur([_,_])),
 	retractall(sureNotMonstre([_,_])),
-	retractall(sureNotMonstreMort([_,_])),
 	retractall(sureNotTrou([_,_])),
 	retractall(knowBruit([_,_])),
 	retractall(knowSouffle([_,_])),
@@ -180,6 +181,95 @@ assignerTrous(Nb) :-
 	assignerTrous(Nb),
 	!.
 
+/*******************************************/
+/**************BOUCLE DE JEU****************/
+/*******************************************/
+
+boucleDeJeu(P) :-
+	(
+            random(1,5,R),
+            tournerJoueur(R),
+            avancerJoueur,
+            afficherJeu(P),
+            sleep(1),
+            boucleDeJeu(P)
+	),
+	!.
+
+/*******************************************/
+/*****************JOUEUR********************/
+/*******************************************/
+
+tournerJoueur(X) :-
+	retractall(direction(_)),
+	assert(direction(X)),
+	!.
+
+bougerJoueur([X,Y]) :-
+	retractall(joueur([_,_])),
+	assert(joueur([X,Y])),
+	!.
+
+avancerJoueur :-
+	getJoueur(X,Y,1,1),
+	getDirection(D,1),
+	(
+	    (D == 1,Xb is X, Yb is Y + 1);
+	    (D == 2,Xb is X + 1, Yb is Y);
+	    (D == 3,Xb is X, Yb is Y - 1);
+	    (D == 4,Xb is X - 1, Yb is Y)
+	),
+	(
+	    not(not(mur([Xb,Yb])));
+	    bougerJoueur([Xb,Yb])
+	),
+	!.
+
+getDirection(D,X) :-
+	X > 4;
+	(
+            not(direction(X)),
+            Xb is X + 1,
+            getDirection(D,Xb)
+	);
+	(
+            not(not(direction(X))),
+            D is X
+	),
+	!.
+
+getJoueur(X,Y,Xa,Ya) :-
+	(
+           (
+	       not(joueur([Xa,Ya])),
+	       Xb is Xa + 1,
+               (
+		   (
+			Xb > 4,
+			Xc is 1,
+			Yc is Ya + 1
+		   );
+		   (
+			Xb < 5,
+			Xc is Xb + 0,
+			Yc is Ya + 0
+		   )
+	       ),
+               getJoueur(X,Y,Xc,Yc)
+	   );
+	   (
+	       not(not(joueur([Xa,Ya]))),
+	       X is Xa + 0,
+	       Y is Ya + 0
+	   );
+	   Ya > 5
+	),
+	!.
+
+/*******************************************/
+/*****************DIVERS********************/
+/*******************************************/
+
 estVide([X,Y]) :-
 	not(monstre([X,Y])),
 	not(mur([X,Y])),
@@ -189,13 +279,18 @@ estVide([X,Y]) :-
 	not(tresor([X,Y])),
 	!.
 
+/*******************************************/
+/****************AFFICHAGE******************/
+/*******************************************/
+
 afficherJeu(P) :-
-        send(P, display, new(T, tabular)),
+	send(P, display, new(T, tabular)),
         send(T, border, 1),
         send(T, cell_spacing, -1),
         send(T, rules, all),
-	afficherElements(T,0,0),
-	send(P, open).
+	afficherElements(T,0,5),
+	send(P, open),
+	send(P,flush).
 
 afficherElements(T,X,Y) :-
 	(
@@ -207,39 +302,40 @@ afficherElements(T,X,Y) :-
                 (
 		        X > 5,
 		        Xb is 0,
-			Yb is Y+1,
+			Yb is Y-1,
 		        send(T,next_row)
 		)
         ),
 	(
-	    Yb < 6,
+	    Yb >= 0,
 	    send(T,append(new(Q, tabular))),
 	    (
+
+		(
+		     not(not(joueur([Xb,Yb]))),
+		     send(Q,append("j", bold, center, valign := center, colour := black, background := cyan, rowspan := 2, colspan := 2))
+	        );
 		(
 		     not(not(monstre([Xb,Yb]))),
-		     send(Q,append("m", bold, center, colour := black, background := green, rowspan := 2, colspan := 2))
+		     send(Q,append("m", bold, center, valign := center, colour := black, background := green, rowspan := 2, colspan := 2))
 	        );
 		(
 		     not(not(tresor([Xb,Yb]))),
-		     send(Q,append("t", bold,  center, colour := black, background := yellow, rowspan := 2, colspan := 2))
+		     send(Q,append("t", bold,  center, valign := center, colour := black, background := yellow, rowspan := 2, colspan := 2))
 	        );
 		(
 		     not(not(ascenseur([Xb,Yb]))),
-		     send(Q,append("a", bold, center, colour := black, background := orange, rowspan := 2, colspan := 2))
-	        );
-		(
-		     not(not(joueur([Xb,Yb]))),
-		     send(Q,append("j", bold, center, colour := black, background := cyan, rowspan := 2, colspan := 2))
+		     send(Q,append("a", bold, center, valign := center, colour := black, background := orange, rowspan := 2, colspan := 2))
 	        );
 		(
 		     not(not(trou([Xb,Yb]))),
-		     send(Q,append("t", bold, center, colour := white, background := brown, rowspan := 2, colspan := 2))
+		     send(Q,append("t", bold, center, valign := center, colour := white, background := brown, rowspan := 2, colspan := 2))
 	        );
 		(
 		     not(not(mur([Xb,Yb]))),
-		     send(Q,append("m", bold, center, colour := white, background := black, rowspan := 2, colspan := 2))
+		     send(Q,append("m", bold, center, valign := center, colour := white, background := black, rowspan := 2, colspan := 2))
 	        );
-		send(Q,append("c", bold, center, colour := white, background := white, rowspan := 2, colspan := 2))
+		send(Q,append("c", bold, center, valign := center, colour := white, background := white, rowspan := 2, colspan := 2))
 	    ),
 	    (
 		(
